83ac45910ce0ceff74d90f8ce60777d9d14cfc96
petercappello <petercappello@192.168.17.186>
2016-03-29 11:03:54 -0700

    Init repo.

---
+++ RmiHello/src/server/HelloServer.java
@@ -0,0 +1,30 @@
+package server;
+
+import api.Hello;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+class HelloServer extends UnicastRemoteObject implements Hello {
+    HelloServer() throws RemoteException {} // "export" HelloServer
+
+    @Override
+    public String hello() { return "Goodby world!"; }
+
+    public static void main( String[] args ) throws RemoteException {
+        // construct & set a security manager (unnecessary in this case)
+        System.setSecurityManager( new SecurityManager() );
+
+        // instantiate a server object
+        Hello hello = new HelloServer(); // can throw RemoteException
+
+        // construct an rmiregistry within this JVM using the default port
+        Registry registry = LocateRegistry.createRegistry( 1099 );
+
+        // bind server in rmiregistry. Can throw exceptions. See api.
+        registry.rebind( Hello.SERVICE_NAME, hello );
+
+        System.out.println("HelloImpl.main: Ready.");
+    }
+}
